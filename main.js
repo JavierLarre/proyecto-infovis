9c760b55d00648f610829d71287df51aa1acc9e8
Osiries1111 <ozdiaz@uc.cl>
2024-10-03 11:55:54 -0300

    djnado una base, hay que modificarlo

---
+++ main.js
@@ -0,0 +1,96 @@
+// LOS DATOS DEBEN ESTAR EN ESTA FORMATO
+/* var trace1 = {
+    x: [1, 2, 3, 4],
+    y: [10, 15, 13, 17],
+    type: 'scatter'
+  };
+  
+  var trace2 = {
+    x: [1, 2, 3, 4],
+    y: [16, 5, 11, 9],
+    type: 'scatter'
+  }; */
+  
+  var data = dataset;
+
+ /*  var layout = {
+    title: 'Title of the Graph',
+    xaxis: {
+      title: 'x-axis title'
+    },
+    yaxis: {
+      title: 'y-axis title'
+    }
+  }; */
+
+  var layout = {
+    showlegend: false,
+    height: 600,
+    width: 600,
+    xaxis: {
+      showline: true,
+      showgrid: false,
+      showticklabels: true,
+      linecolor: 'rgb(204,204,204)',
+      linewidth: 2,
+      autotick: false,
+      ticks: 'outside',
+      tickcolor: 'rgb(204,204,204)',
+      tickwidth: 2,
+      ticklen: 5,
+      tickfont: {
+        family: 'Arial',
+        size: 12,
+        color: 'rgb(82, 82, 82)'
+      }
+    },
+    yaxis: {
+      showgrid: false,
+      zeroline: false,
+      showline: false,
+      showticklabels: false
+    },
+    autosize: false,
+    margin: {
+      autoexpand: false,
+      l: 100,
+      r: 20,
+      t: 100
+    },
+    annotations: [
+      {
+        xref: 'paper',
+        yref: 'paper',
+        x: 0.0,
+        y: 1.05,
+        xanchor: 'left',
+        yanchor: 'bottom',
+        text: 'Main Source for News',
+        font:{
+          family: 'Arial',
+          size: 30,
+          color: 'rgb(37,37,37)'
+        },
+        showarrow: false
+      },
+      {
+        xref: 'paper',
+        yref: 'paper',
+        x: 0.5,
+        y: -0.1,
+        xanchor: 'center',
+        yanchor: 'top',
+        text: 'Source: Pew Research Center & Storytelling with data',
+        showarrow: false,
+        font: {
+          family: 'Arial',
+          size: 12,
+          color: 'rgb(150,150,150)'
+        }
+      }
+    ]
+  };
+  
+  Plotly.newPlot('myDiv', data, layout);
+
+
