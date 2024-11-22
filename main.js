6b7d4897c784acaa3cc8364a718e960b7d192113
JavierLarre <javier.larre@uc.cl>
2024-11-22 12:37:55 -0300

    turbo refactorizacion

--- main.js
+++ main.js
@@ -1,50 +1,6 @@
 import { plotData } from "./src/plot/plot.js";
-import { AR } from "./src/aruco/aruco.js";
+import { aruco } from "./src/aruco/aruco.js";
 
 await plotData();
 
-const detector = new AR.Detector();
-// Function to process the video frame and detect markers
-function detectMarkers(imageData) {
-    const markers = detector.detect(imageData);
-    if (markers.length > 0) {
-        const marker = markers[0];
-        const div = document.getElementById('aruco-detected');
-        div.innerHTML = `ID: ${marker.id} AT: ${marker.corners[0].x}, ${marker.corners[0].y}`;
-    }
-}
-
-function init() {
-    const video = document.getElementById('video') ;
-    const canvas = document.getElementById('canvas');
-    const context = canvas.getContext('2d');
-
-    // Capture the video frame and detect markers
-    video.addEventListener('play', () => {
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-
-        function processFrame() {
-            if (video.paused || video.ended) {
-                return;
-            }
-            context.drawImage(video, 0, 0, canvas.width, canvas.height);
-            const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-
-            detectMarkers(imageData);
-            requestAnimationFrame(processFrame);
-        }
-        processFrame();
-    });
-
-    // Access the webcam
-    navigator.mediaDevices.getUserMedia({ video: true })
-        .then(stream => {
-            video.srcObject = stream;
-        })
-        .catch(err => {
-            console.error(err.name + ": " + err.message);
-        });
-}
-
-init();
+aruco.start();
